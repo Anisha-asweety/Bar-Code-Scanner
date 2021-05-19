cd1f0a94c494b9e7889a01e733593cfdc879616e
Anisha-asweety <70945347+Anisha-asweety@users.noreply.github.com>
2021-05-19 17:34:36 +0530

    Add files via upload

---
+++ App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import ScanScreen from './Screens/ScanScreen';
+
+export default function App() {
+  return (
+    <View>
+      
+    </View>
+  );
+}
